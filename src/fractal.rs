ddf9ef55a5426570f75e18b206ea6cec5f98fd88
Stijn Van Horenbeek <stijn.van.horenbeek@outlook.com>
2023-08-22 00:46:55 +0200

    get some async in this bitch

--- src/fractal.rs
+++ src/fractal.rs
@@ -13,3 +13,32 @@ pub fn mandelbrot(c: Complex<f64>) -> u32 {
     }
     MAX_ITER
 }
+
+pub async fn compute_fractal(
+    center_x: f64,
+    center_y: f64,
+    width: usize,
+    height: usize,
+) -> Vec<Vec<u32>> {
+    let rows: Vec<_> = (0..height)
+        .map(|y| {
+            tokio::spawn(async move {
+                let mut row = Vec::with_capacity(width);
+                for x in 0..width {
+                    let cx = ((x as f64 / width as f64) * 4.0 - 2.0) + center_x;
+                    let cy = ((y as f64 / height as f64) * 4.0 - 2.0) + center_y;
+                    let c = num_complex::Complex::new(cx, cy);
+                    let val = mandelbrot(c);
+                    row.push(val);
+                }
+                row
+            })
+        })
+        .collect();
+
+    let mut result = Vec::with_capacity(height);
+    for row_fut in rows {
+        result.push(row_fut.await.unwrap());
+    }
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ mod controller;
 mod fractal;
 mod renderer;
 use controller::{Controller, Direction};
-use fractal::mandelbrot;
 use sdl2::pixels::Color;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let sdl_context = sdl2::init().unwrap();
     let mut renderer = renderer::Renderer::new(&sdl_context);
     let mut controller = Controller::new(&sdl_context);
@@ -28,15 +28,20 @@ fn main() {
                 Direction::Right => center_x += MOVE_SPEED,
             }
         }
-        for x in 0..renderer::WIDTH {
-            for y in 0..renderer::HEIGHT {
-                let cx = ((x as f64 / renderer::WIDTH as f64) * 4.0 - 2.0) + center_x;
-                let cy = ((y as f64 / renderer::HEIGHT as f64) * 4.0 - 2.0) + center_y;
-                let c = num_complex::Complex::new(cx, cy);
-                let val = mandelbrot(c);
-                let color_value = (val * 255 / fractal::MAX_ITER) as u8;
-                let color = Color::RGB(color_value, color_value, color_value);
-                renderer.draw_point(x as i32, y as i32, color);
+
+        let fractal_data = fractal::compute_fractal(
+            center_x,
+            center_y,
+            renderer::WIDTH as usize,
+            renderer::HEIGHT as usize,
+        )
+        .await;
+
+        for (y, row) in fractal_data.iter().enumerate() {
+            for (x, &val) in row.iter().enumerate() {
+                let color = (val as f64 * 255.0 / fractal::MAX_ITER as f64) as u8;
+                let color = Color::RGB(color, color, color);
+                renderer.draw_point(x as i32, y as i32, color)
             }
         }
         renderer.canvas.present();
